ee09b305acdf96e108cc5b7eff7b79c4832e6117
codecraftlearning <codecraftlearning@gmail.com>
2025-06-09 17:50:47 +0530

    seo add and responsive fix

--- codecraftlearningproject/src/app/app-routing.module.ts
+++ codecraftlearningproject/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
@@ -21,6 +21,7 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes, {
     scrollPositionRestoration: 'enabled',
     anchorScrolling: 'enabled',
+    preloadingStrategy: PreloadAllModules 
   })],
   exports: [RouterModule]
 })
