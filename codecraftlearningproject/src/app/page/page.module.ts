06060ff4c7ad0dbb069cbbb5423207bc1967f969
codecraftlearning <codecraftlearning@gmail.com>
2025-06-12 02:18:50 +0530

    seo performance upgrade

--- codecraftlearningproject/src/app/page/page.module.ts
+++ codecraftlearningproject/src/app/page/page.module.ts
@@ -20,16 +20,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TimestampPipePipe } from '../pipe/timestamp-pipe.pipe';
 import { CollaborationComponent } from './collaboration/collaboration.component';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCbfNfncyea0XyhKQkmzURjXHIkDVC1HbA",
-  authDomain: "codecraftlearning-user.firebaseapp.com",
-  projectId: "codecraftlearning-user",
-  storageBucket: "codecraftlearning-user.firebasestorage.app",
-  messagingSenderId: "770678064720",
-  appId: "1:770678064720:web:834b435d4c480ab1cc3355"
-};
-
-
 export const standaloneComponents = [
   HeaderComponent,
   AboutComponent,
